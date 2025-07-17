d91d85e95a9b9530231d56855bac7a7c4c3e09c3
Ashish Kumar <ashish.kumar@agribazaar.com>
2025-07-17 15:05:38 +0530

    Added new Questions

--- src/quizData.js
+++ src/quizData.js
@@ -2,7 +2,7 @@ export const quizData = {
   categories: [
     {
       id: 'js_basics',
-      name: 'JavaScript Basics',
+      name: 'JavaScript Basics (5 Questions)',
       questions: [
         {
           id: 'q1',
@@ -13,37 +13,121 @@ export const quizData = {
             "C. <script src='script.js'>",
             "D. <script file='script.js'>"
           ],
-          correctAnswer: 'C',
+          correctAnswer: "C",
           timeLimit: 10
         },
         {
           id: 'q2',
-          question: 'Which company developed JavaScript?',
+          question: "Which company developed JavaScript?",
           options: [
-            'A. Microsoft',
-            'B. Netscape',
-            'C. Google',
-            'D. Mozilla'
+            "A. Microsoft",
+            "B. Netscape",
+            "C. Google",
+            "D. Mozilla"
           ],
-          correctAnswer: 'B',
+          correctAnswer: "B",
+          timeLimit: 10
+        },
+        {
+          id: 'q3',
+          question: "Inside which HTML element do we put the JavaScript?",
+          options: [
+            "A. <javascript>",
+            "B. <js>",
+            "C. <script>",
+            "D. <code>"
+          ],
+          correctAnswer: "C",
+          timeLimit: 10
+        },
+        {
+          id: 'q4',
+          question: "How do you write \"Hello World\" in an alert box?",
+          options: [
+            "A. msg('Hello World');",
+            "B. alertBox('Hello World');",
+            "C. alert('Hello World');",
+            "D. msgBox('Hello World');"
+          ],
+          correctAnswer: "C",
+          timeLimit: 10
+        },
+        {
+          id: 'q5',
+          question: "How to write an IF statement in JavaScript?",
+          options: [
+            "A. if i = 5 then",
+            "B. if i == 5 then",
+            "C. if (i == 5)",
+            "D. if i = 5"
+          ],
+          correctAnswer: "C",
           timeLimit: 10
         }
       ]
     },
     {
       id: 'react_basics',
-      name: 'React Basics',
+      name: 'React Basics (5 Questions)',
       questions: [
         {
-          id: 'q1',
-          question: 'What is the correct syntax to create a functional component in React?',
+          id: 'q6',
+          question: "What is React?",
           options: [
-            'A. function MyComponent() {}',
-            'B. class MyComponent extends React.Component {}',
-            'C. var MyComponent = React.createClass({})',
-            'D. new MyComponent()'
+            "A. A JavaScript library for building user interfaces",
+            "B. A CSS framework",
+            "C. A database",
+            "D. A server-side language"
           ],
-          correctAnswer: 'A',
+          correctAnswer: "A",
+          timeLimit: 10
+        },
+        {
+          id: 'q7',
+          question: "What is the command to create a new React app?",
+          options: [
+            "A. npx create-react-app myApp",
+            "B. npm init react",
+            "C. create-react myApp",
+            "D. react-create myApp"
+          ],
+          correctAnswer: "A",
+          timeLimit: 10
+        },
+        {
+          id: 'q8',
+          question: "What is JSX?",
+          options: [
+            "A. A syntax extension for JavaScript",
+            "B. A new programming language",
+            "C. JSON with XML",
+            "D. JavaScript XML transformer"
+          ],
+          correctAnswer: "A",
+          timeLimit: 10
+        },
+        {
+          id: 'q9',
+          question: "Which hook is used to manage state in functional components?",
+          options: [
+            "A. useEffect",
+            "B. useContext",
+            "C. useReducer",
+            "D. useState"
+          ],
+          correctAnswer: "D",
+          timeLimit: 10
+        },
+        {
+          id: 'q10',
+          question: "How do you pass data from a parent to a child component?",
+          options: [
+            "A. Using props",
+            "B. Using useState",
+            "C. Using context",
+            "D. Using Redux"
+          ],
+          correctAnswer: "A",
           timeLimit: 10
         }
       ]
